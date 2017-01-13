97868285be10078a72888f37b23ca3112cef5a54
rchekalov <rchekalov@me.com>
2017-01-13 10:03:19 +0300

    add saving id read posts

--- crawler/crawler_rss.py
+++ crawler/crawler_rss.py
@@ -5,6 +5,9 @@ import aiohttp
 import asyncio
 import feedparser
 from crawler.abc_crawler import ABCrawler
+from emoji import emojize
+import aioredis
+import os
 
 class RSSCrawler(ABCrawler):
 
@@ -21,7 +24,7 @@ class RSSCrawler(ABCrawler):
                     headers=resp.headers
                 )
 
-    async def worker(self, feed, semaphore):
+    async def worker(self, feed, semaphore, redis):
         with (await semaphore):
             rss = feedparser.parse(await self._download_rss(feed['url'], semaphore))
         for entry in rss.entries:
@@ -36,9 +39,13 @@ class RSSCrawler(ABCrawler):
                 'title': html.escape(entry.get('title', '')),
                 'url': entry['id']
             }
-            result = Template(feed['format']).safe_substitute(template_dict)
-            with (await semaphore):
-                await self.answer_callback(result)
+            result = emojize(Template(feed['format']).safe_substitute(template_dict))
+            if not redis.sismember('rss_id_set', entry['id']):
+                redis.sadd('rss_id_set', entry['id'])
+                with (await semaphore):
+                    await self.answer_callback(result)
 
     async def crawl(self, config, semaphore):
-        [asyncio.ensure_future(self.worker(feed, semaphore)) for feed in config['feeds']]
+        host = os.environ.get('REDIS_HOST', 'localhost')
+        redis = await aioredis.create_redis((host, 6379), encoding="utf-8")
+        [asyncio.ensure_future(self.worker(feed, semaphore, redis)) for feed in config['feeds']]
